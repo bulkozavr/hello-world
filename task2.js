a9e2d4ac5dbd9eaf05908047e13c5b377e1c0103
bulkozavr <tom.smith.2013@mail.ru>
2018-06-16 16:49:13 +0300

    Update task2.js
    This variant pass 8 tests vs 0 tests of new idea. Let it be.

--- task2.js
+++ task2.js
@@ -1,28 +1,21 @@
 var s = process.argv[2];
 
-var max = 0, start;
-for(var k = 0; k < n; ++k){
-	var tmpS = s.substring(k);
-	if (tmpS.length <= max)
-		break;
-	m = tmpS.length;
-	var pi = [m];
-	pi[0] = 0
-	for (var i = 1; i < m; ++i) {
-		var j = pi[i - 1];
-		while (j > 0 && tmpS[i] != tmpS[j])
-			j = pi[j - 1];
-		if (tmpS[i] == tmpS[j]) 
-			++j;
-		pi[i] = j;
-	}
-	for (var i = 0; i < m; ++i) {
-	  if (pi[i] > max){
-		max = pi[i];
-		start = k;
-	  }
-	}
-	pi = [];
+var n = s.length;
+var pi = [n];
+
+pi[0] = 0
+for (var i=1; i < n; ++i) {
+	var j = pi[i - 1];
+	while (j > 0 && s[i] != s[j])
+	j = pi[j - 1];
+	if (s[i] == s[j]) 
+		++j;
+	pi[i] = j;
 }
 
-console.log(s.substr(start, max));
+var max = 0;
+for (var i = 0; i < n; ++i) {
+  if (pi[i] > max)
+    max = pi[i];
+}
+console.log(s.substr(0, max));
